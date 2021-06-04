0b1d8ee751b95b30d029e08dac0ee7e8f14d8460
koralx <miron.miron.y@mail.ru>
2021-06-04 20:30:06 +0500

    lox

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 let port = 25300
